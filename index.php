b0e12bb6e9f0a4b7bfcd8a67331e6e56860f0f1a
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-08-22 15:07:04 +0200

    add table visualization for data

--- index.php
+++ index.php
@@ -45,24 +45,74 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--bootstrap-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <!--style css-->
   <link rel="stylesheet" href="./CSS/style.css">
+  
   <title>PHP Hotel</title>
 </head>
 <body class="index-body">
-    <?php 
-      foreach ($hotels as $hotel) { 
-    ?>
-      <div>
-        <?php  
-          foreach ($hotel as $key => $data) { ?>
-            <p><?= $key.': '.$data  ?></p>
-            <?php  
-          } 
+    <div class="container">
+      <h1 class="text-center mt-5">Hotel</h1>
+
+      <table class="table table-bordered mt-5">
+        <thead>
+          <tr>
+            <?php foreach ($hotels[0] as $key => $value) { ?>
+              <th>
+                  <?= $key ?> 
+              </th>        
+            <?php
+              }
+            ?>
+             
+          </tr>
+        </thead>
+        <tbody>
+        <?php 
+          foreach ($hotels as $hotel) { 
         ?>
-      </div>
-    <?php
-      }
-    ?>
+          <tr>
+            <?php 
+              foreach ($hotel as $key => $data) { 
+            ?>
+              <td>
+                <?php 
+                  if ($data == 1 && $key == 'parking') {
+                
+                    echo "Si" ;
+                
+                  }elseif($data == "" && $key == 'parking') {
+                  
+                    echo "No";
+                  
+                  }elseif($key == 'distance_to_center'){
+
+                    echo '<strong>'.$data.'</strong> km';
+                  
+                  }elseif($key == 'name'){
+                
+                    echo '<strong class="text-danger">'.$data.'</strong>';
+                
+                  }else{
+                
+                    echo $data;
+                
+                  }
+                ?>
+              </td>
+            <?php
+              }
+            ?>
+          </tr>
+        <?php
+          }
+        ?>
+        </tbody>
+      </table>
+    
+
+    </div>
 </body>
 </html>
